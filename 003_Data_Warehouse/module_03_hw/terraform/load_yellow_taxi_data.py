92500eca78804f7c0ba8d6055af92e5ec07ac0ac
justigo86 <justigo86@gmail.com>
2026-02-10 23:08:54 -0500

    started module 4 - installed duckdb and dbt with duckdb - initialized dbt project

--- 003_Data_Warehouse/module_03_hw/terraform/load_yellow_taxi_data.py
+++ 003_Data_Warehouse/module_03_hw/terraform/load_yellow_taxi_data.py
@@ -3,7 +3,7 @@ import sys
 import urllib.request
 from concurrent.futures import ThreadPoolExecutor
 from google.cloud import storage
-from google.api_core.exceptions import NotFound, Forbidden
+# from google.api_core.exceptions import NotFound, Forbidden
 import time
 
 
