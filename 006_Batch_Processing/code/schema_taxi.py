3995c90e81f775aac33f16dcc04c8544f590bc76
justigo86 <justigo86@gmail.com>
2026-03-06 23:14:04 -0500

    after previously completing green data schema - now completed the yellow pyspark df schema

--- 006_Batch_Processing/code/schema_taxi.py
+++ 006_Batch_Processing/code/schema_taxi.py
@@ -22,3 +22,24 @@ t.StructType([
   t.StructField('trip_type', t.IntegerType(), True), 
   t.StructField('congestion_surcharge', t.DoubleType(), True)
 ])
+
+t.StructType([
+  t.StructField('VendorID', t.IntegerType(), True),
+  t.StructField('tpep_pickup_datetime', t.TimestampType(), True),
+  t.StructField('tpep_dropoff_datetime', t.TimestampType(), True),
+  t.StructField('passenger_count', t.IntegerType(), True),
+  t.StructField('trip_distance', t.DoubleType(), True),
+  t.StructField('RatecodeID', t.IntegerType(), True),
+  t.StructField('store_and_fwd_flag', t.StringType(), True),
+  t.StructField('PULocationID', t.IntegerType(), True),
+  t.StructField('DOLocationID', t.IntegerType(), True),
+  t.StructField('payment_type', t.IntegerType(), True),
+  t.StructField('fare_amount', t.DoubleType(), True),
+  t.StructField('extra', t.DoubleType(), True),
+  t.StructField('mta_tax', t.DoubleType(), True),
+  t.StructField('tip_amount', t.DoubleType(), True),
+  t.StructField('tolls_amount', t.DoubleType(), True),
+  t.StructField('improvement_surcharge', t.DoubleType(), True),
+  t.StructField('total_amount', t.DoubleType(), True),
+  t.StructField('congestion_surcharge', t.DoubleType(), True)
+])
