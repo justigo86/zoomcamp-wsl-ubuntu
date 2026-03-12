0e38c3b9a8d4959a6dd3619dc3c463491186d632
justigo86 <justigo86@gmail.com>
2026-03-11 23:33:09 -0400

    working through module 7 lecture - just completed the producer and adjacent models.py file

---
+++ 007_Streaming/workshop/notebooks/models.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+import json
+import dataclasses
+
+# create a class to ensure datatypes utilized
+@dataclass
+class Ride:
+    PULocationID: int
+    DOLocationID: int
+    trip_distance: float
+    total_amount: float
+    tpep_pickup_datetime: int  # epoch milliseconds
+
+# function to convert a df row into a "ride"
+# transforming timestamp into epoch milliseconds for Flink later
+def ride_from_row(row):
+    return Ride(
+        PULocationID=int(row['PULocationID']),
+        DOLocationID=int(row['DOLocationID']),
+        trip_distance=float(row['trip_distance']),
+        total_amount=float(row['total_amount']),
+        tpep_pickup_datetime=int(row['tpep_pickup_datetime'].timestamp() * 1000),
+    )
+
+# function to incorporate dataclasses directly
+def ride_serializer(ride):
+    ride_dict = dataclasses.asdict(ride)
+    json_str = json.dumps(ride_dict)
+    return json_str.encode('utf-8')
