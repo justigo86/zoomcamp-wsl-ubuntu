107353b973e9cbfa99c4f471df29557552107332
justigo86 <justigo86@gmail.com>
2026-03-22 01:21:25 -0400

    zoomcamp - started final project - cannot apply terraform config yet as waiting for gcp to update info - will try again soon

---
+++ 008_Course_Project/terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+resource "google_storage_bucket" "proj-dl-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 90
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+resource "google_bigquery_dataset" "proj_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
---
+++ 008_Course_Project/terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/my-creds.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "zoomcamp-final-project"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "zoomcamp-proj-bq-bucket0326"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "proj_bq_dataset"
+}
